3d7a231828d72c744fb31c1c95416da2f52cb670
gabbar23 <amansaini842@gmail.com>
2022-08-02 20:14:43 +0530

    new features

--- starter/script.js
+++ starter/script.js
@@ -10,3 +10,38 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              autoClose: false,
+              closeOnClick: false,
+              className:"running-popup"
+            })
+          ).setPopupContent("workout")
+          .openPopup();
+      });
+    },
+    function () {
+      alert('no location found');
+    }
+  );
+}
